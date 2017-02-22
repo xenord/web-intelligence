f0a66aadfc2aa590400d44fc1ec5ca74481e7524
Francesco Benetello <francesco.benetello921@gmail.com>
2017-02-22 15:37:25 +0100

    Added as comments JSON parser

--- SecondaConsegna/Codice/fpgrowth.py
+++ SecondaConsegna/Codice/fpgrowth.py
@@ -1,13 +1,20 @@
 from pyspark.mllib.fpm import FPGrowth
 from pyspark import SparkContext
+import json
 
 sc=SparkContext()
 
-data = sc.textFile("10M_Tweets.txt")
+data = sc.textFile("/Users/francescobenetello/Documents/Dataset/sample.txt")
+#openJSON = sc.textFile('/Users/francescobenetello/Documents/Dataset/sample.json')
+'''
+def f(linea):
+	return json.loads(linea)["text"]
 
+data = openJSON.map(lambda linea: json.loads(linea)["text"])
+'''
 transactions = data.map(lambda line: line.split(' '))
 unique= transactions.map(lambda x: list(set(x))).cache()
-model = FPGrowth.train(unique, minSupport=0.01, numPartitions=5)
+model = FPGrowth.train(unique, minSupport=0.01, numPartitions=2)
 
 result = model.freqItemsets().collect()
 
