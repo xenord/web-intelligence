a09ad403060a6103050b64a199ea0dc8c5c3afbd
Francesco Benetello <francesco.benetello921@gmail.com>
2017-02-23 12:08:43 +0100

    ADD: time counter

--- SecondaConsegna/Codice/fpgrowth.py
+++ SecondaConsegna/Codice/fpgrowth.py
@@ -1,23 +1,22 @@
 from pyspark.mllib.fpm import FPGrowth
 from pyspark import SparkContext
-import json
+from datetime import datetime
 
 sc=SparkContext()
 
 data = sc.textFile("/Users/francescobenetello/Documents/Dataset/sample.txt")
-#openJSON = sc.textFile('/Users/francescobenetello/Documents/Dataset/sample.json')
-'''
-def f(linea):
-	return json.loads(linea)["text"]
 
-data = openJSON.map(lambda linea: json.loads(linea)["text"])
-'''
 transactions = data.map(lambda line: line.split(' '))
 unique= transactions.map(lambda x: list(set(x))).cache()
-model = FPGrowth.train(unique, minSupport=0.01, numPartitions=2)
+START_TIME = datetime.now()
+model = FPGrowth.train(unique, minSupport=0.01, numPartitions=16)
+END_TIME = datetime.now()
 
 result = model.freqItemsets().collect()
 
 for fi in result:
     print(fi)
+
+TIME = format(END_TIME-START_TIME)
+print("Tempo di esecuzione: " + str(TIME) + " secs") 
 
