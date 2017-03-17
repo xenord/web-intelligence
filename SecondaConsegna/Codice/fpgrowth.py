92aa6b2bf43c576b75bd0e55c062f86d23e6bf00
Francesco Benetello <francesco.benetello921@gmail.com>
2017-03-17 14:19:58 +0100

    Removed unused functions

--- SecondaConsegna/Codice/fpgrowth.py
+++ SecondaConsegna/Codice/fpgrowth.py
@@ -1,15 +1,20 @@
 from pyspark.mllib.fpm import FPGrowth
 from pyspark import SparkContext
 from datetime import datetime
+import json
+import re
+import io
 
 sc=SparkContext()
 
-data = sc.textFile("/Users/francescobenetello/Documents/Dataset/sample.txt")
 
-transactions = data.map(lambda line: line.split(' '))
-unique= transactions.map(lambda x: list(set(x))).cache()
+data = sc.textFile('/Users/francescobenetello/Documents/Dataset/simplest_sample.txt')
+
+transactions = data.map(lambda line: line.strip().split(' '))
+unique= transactions.map(lambda x: list(set(x)))
+
 START_TIME = datetime.now()
-model = FPGrowth.train(unique, minSupport=0.01, numPartitions=16)
+model = FPGrowth.train(unique, minSupport=0.0001, numPartitions=2)
 END_TIME = datetime.now()
 
 result = model.freqItemsets().collect()
@@ -18,5 +23,4 @@ for fi in result:
     print(fi)
 
 TIME = format(END_TIME-START_TIME)
-print("Tempo di esecuzione: " + str(TIME) + " secs") 
-
+print("Tempo di esecuzione: " + str(TIME) + " sec")
